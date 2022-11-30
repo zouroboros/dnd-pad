6a4f12a85578aa319d43cfc6c1ff703e977def94
Zouroboros <zouroboros@zoho.eu>
2022-11-30 14:48:18 +0100

    Refactored character sheet in character component.

--- src/App.svelte
+++ src/App.svelte
@@ -2,19 +2,7 @@
 	import equal from 'deep-equal';
 
 	import { afterUpdate } from 'svelte';
-	import CharacterName from './CharacterName.svelte';
-	import CharacterPanel from './CharacterPanel.svelte';
-	import CharacterStats from './CharacterStats.svelte';
-	import SingleState from './SingleStat.svelte';
-	import SavingThrows from './SavingThrows.svelte';
-	import Skills from './Skills.svelte';
-	import HitPoints from './HitPoints.svelte';
-	import BackgroundInfos from './BackgroundInfos.svelte';
-	import Attacks from './Attacks.svelte';
-	import Spells from './Spells.svelte';
-	import PropertyList from './PropertyList.svelte';
-	import Equipment from './Equipment.svelte';
-	import Notes from './Notes.svelte';
+    import Character from './Character.svelte';
 
 	let upgradeCharacter = function(characterObject) {
 		if(!characterObject) {
@@ -98,9 +86,6 @@
 </script>
 
 <style>
-.character-sheet > div {
-	margin-bottom: 2rem;
-}
 </style>
 
 <header class="hide-on-print">
@@ -111,57 +96,5 @@
 	{/if}
 </header>
 <main>
-	<form class="character-sheet gothic">
-			<h1>Character</h1>
-			<div>
-				<CharacterName bind:character={character} />
-			</div>
-			<div>
-				<CharacterPanel bind:character={character} />
-			</div>
-			<div>
-				<CharacterStats bind:character={character} />
-			</div>
-			<div>
-				<SingleState name={'Inspiration'} bind:stat={character.inspiration} />
-			</div>
-			<div>
-				<SingleState name={'Proficiency Bonus'} bind:stat={character.proficiency} />
-			</div>
-			<div>
-				<SavingThrows bind:saving={character.saving}/>
-			</div>
-			<div>
-				<Skills bind:character={character}/>
-			</div>
-			<div>
-				<SingleState name={'Passive Wisdom (Perception)'} bind:stat={character.passiveWisdom} />
-			</div>
-			<div>
-				<HitPoints bind:character={character}/>
-			</div>
-			<div>
-				<BackgroundInfos bind:character={character}/>
-			</div>
-			<div>
-				<Attacks bind:character={character} />
-			</div>
-			<div>
-				<Spells bind:character={character} />
-			</div>
-			<div>
-				<h2>Other Proficiencies &amp; Languages</h2>
-				<PropertyList bind:properties={character.otherProficiencies}/>
-			</div>
-			<div>
-				<Equipment bind:character={character}/>
-			</div>
-			<div>
-				<h2>Features & Traits</h2>
-				<PropertyList bind:properties={character.featuresAndTraits}/>
-			</div>
-			<div>
-				<Notes bind:notes={character.notes} />
-			</div>
-	</form>
+	<Character character={character}></Character>
 </main>
